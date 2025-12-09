2d6d0b84554fc0439941d97df709512faf262706
Mark <markangelo.templanza.elyon@gmail.com>
2025-12-09 10:28:51 +0800

    fixed hr leave bug for Aaron access rights error.

--- ace-saatchi-consultant-test/base_customization/models/inherit.py
+++ ace-saatchi-consultant-test/base_customization/models/inherit.py
@@ -721,7 +721,8 @@ class HrLeave(models.Model):
         # Check if employee_id and holiday_status_id are in vals
         if vals.get('employee_id') and vals.get('holiday_status_id') == 5:
             # Get the employee record
-            employee = self.env['hr.employee'].browse(vals.get('employee_id'))
+            employee = self.env['hr.employee'].sudo().browse(
+                vals.get('employee_id'))
 
             # Check if employment start date is set
             if employee.x_studio_employment_start_date:
