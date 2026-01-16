8356273c57d96611ef750f337f8d440374d57198
Mark <markangelo.templanza.elyon@gmail.com>
2026-01-16 02:16:43 +0000

    Added Aged Receivable

--- ace-saatchi-consultant-test/saatchi_soa/reports/__init__.py
+++ ace-saatchi-consultant-test/saatchi_soa/reports/__init__.py
@@ -2,3 +2,4 @@ from . import soa_xlsx_report
 from . import accrued_revenue_xlsx_report
 from . import unbilled_estimate_report
 from . import accrued_revenue_adjustment_report
+from . import aged_receivable_report
